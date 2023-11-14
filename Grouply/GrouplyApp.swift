b5259f826d485d6277795587a554191948924e86
bwondessen <bwondessen23@gmail.com>
2023-11-13 18:05:47 -0600

    Initial Commit

---
+++ Grouply/GrouplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrouplyApp.swift
+//  Grouply
+//
+//  Created by Bruke Wondessen on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct GrouplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
