0d8520736f38c7ae63aa7d261de823b988639194
bwondessen <bwondessen23@gmail.com>
2023-11-16 18:44:18 -0600

    Create basic layout for settings view

---
+++ Grouply/View/Settings/SettingsHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsHeaderView.swift
+//  Grouply
+//
+//  Created by Bruke Wondessen on 11/15/23.
+//
+
+import SwiftUI
+
+struct SettingsHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsHeaderView()
+}
