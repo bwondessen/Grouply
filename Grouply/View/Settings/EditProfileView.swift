67b1c36ee7e59f509f2522c4f378d7ee15ae5fcc
bwondessen <bwondessen23@gmail.com>
2023-11-19 00:05:20 -0600

    Build edit profile view in settings

---
+++ Grouply/View/Settings/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Grouply
+//
+//  Created by Bruke Wondessen on 11/16/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
