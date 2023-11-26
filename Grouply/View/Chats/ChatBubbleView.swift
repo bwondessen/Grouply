f7cc96f15f698cb0ae7e6a444cd9029c3be0b43e
bwondessen <bwondessen23@gmail.com>
2023-11-26 01:04:32 -0600

    Build custom chat bubble

---
+++ Grouply/View/Chats/ChatBubbleView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatBubbleView.swift
+//  Grouply
+//
+//  Created by Bruke Wondessen on 11/25/23.
+//
+
+import SwiftUI
+
+struct ChatBubbleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatBubbleView()
+}
