585d2718f60cc39ab8722a1345923e9ff87e3527
bwondessen <bwondessen23@gmail.com>
2023-11-26 01:06:14 -0600

    Build message view

---
+++ Grouply/View/Chats/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  Grouply
+//
+//  Created by Bruke Wondessen on 11/26/23.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
