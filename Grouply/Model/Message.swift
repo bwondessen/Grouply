cc205abd39717323ddfa2c76c57b2e4d618747ab
bwondessen <bwondessen23@gmail.com>
2023-11-27 18:27:18 -0600

    Create message model and chat view model

---
+++ Grouply/Model/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  Grouply
+//
+//  Created by Bruke Wondessen on 11/27/23.
+//
+
+import Foundation
---
+++ Grouply/ViewModel/ChatViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatViewModel.swift
+//  Grouply
+//
+//  Created by Bruke Wondessen on 11/27/23.
+//
+
+import Foundation
