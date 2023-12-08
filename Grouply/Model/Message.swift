30f67ec769be72a076f890a1ffe75d00a4d061c5
bwondessen <bwondessen23@gmail.com>
2023-12-07 20:42:44 -0600

    Reconfigure message model to reflect message object in backend

--- Grouply/Model/Message.swift
+++ Grouply/Model/Message.swift
@@ -5,10 +5,14 @@
 //  Created by Bruke Wondessen on 11/27/23.
 //
 
-import Foundation
+import FirebaseFirestoreSwift
+import Firebase
 
-struct Message: Identifiable {
-    let id = NSUUID().uuidString
-    let isFromCurrentUser: Bool
-    let messageText: String
+struct Message: Identifiable, Decodable {
+    @DocumentID var id: String?
+    let fromId: String
+    let toId: String
+    let read: Bool
+    let text: String
+    let timeStamp: Timestamp
 }
